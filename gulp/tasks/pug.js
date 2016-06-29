26ba46e04b35901ce737eedc46856241ca69264a
Maxence Busson <mbusson@users.noreply.github.com>
2016-06-29 13:50:52 +0200

    Add files via upload

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -5,7 +5,7 @@ var pug = require('gulp-pug');
 var gutil = require('gulp-util');
 
 gulp.task('pug', function() {
-    gulp.src('pug/*.pug')
+    gulp.src('pug/**/*.pug')
         .pipe(pug().on('error', gutil.log))
         .pipe(gulp.dest('templates/'))
 });
